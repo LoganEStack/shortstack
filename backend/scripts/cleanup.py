741562978e6eb01c5ae2efa13a8986f537dae1bc
Logan Stack <loganestack@gmail.com>
2025-05-31 16:46:01 -0400

    Adding print statement to cron job.

--- backend/scripts/cleanup.py
+++ backend/scripts/cleanup.py
@@ -8,6 +8,7 @@ with app.app_context():
     now = datetime.now(timezone.utc)
     expired_urls = URL.query.filter(URL.expiration_date < now).all()
     for url in expired_urls:
+        print(" - Deleting", url)
         db.session.delete(url)
     db.session.commit()
     print(f"Deleted {len(expired_urls)} expired URLs.")
